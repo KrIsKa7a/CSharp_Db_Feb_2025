15f07f89a0aafa0da7f75b4e1e3ecc55bfc73722
Kristiyan Ivanov <kristiqn.ivanov@abv.bg>
2025-03-06 22:29:54 +0200

    Upload serialization and deserialization logic.

--- E06_JSON_Proccessing/ProductShop/Data/Configuration.cs
+++ E06_JSON_Proccessing/ProductShop/Data/Configuration.cs
@@ -3,6 +3,6 @@
     public static class Configuration
     {
         public const string ConnectionString =
-            @"Server=DESKTOP-H3STVF4\SQLEXPRESS;Database=ProductShop2025;Integrated Security=True";
+            @"Server=.;Database=ProductShop2025;Integrated Security=True";
     }
 }
