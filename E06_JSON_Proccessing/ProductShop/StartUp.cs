fb1fbc70448b72f9bcb85664faa737c901f52173
Kristiyan Ivanov <kristiqn.ivanov@abv.bg>
2025-03-06 19:18:24 +0200

    Add initial DB migration.

--- E06_JSON_Proccessing/ProductShop/StartUp.cs
+++ E06_JSON_Proccessing/ProductShop/StartUp.cs
@@ -1,10 +1,16 @@
 ﻿namespace ProductShop
 {
+    using Microsoft.EntityFrameworkCore;
+
+    using Data;
+
     public class StartUp
     {
         public static void Main()
         {
-
+            using ProductShopContext dbContext = new ProductShopContext();
+            dbContext.Database.Migrate();
+            Console.WriteLine("Database migrated successfully!");
         }
     }
 }
