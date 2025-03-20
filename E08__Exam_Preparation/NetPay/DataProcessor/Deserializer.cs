4248ef235bac2f39fb95ebfaf5c430f110f31323
Kristiyan Ivanov <kristiqn.ivanov@abv.bg>
2025-03-20 19:00:54 +0200

    Initial skeleton upload with refactoring.

---
+++ E08__Exam_Preparation/NetPay/DataProcessor/Deserializer.cs
@@ -0,0 +1,35 @@
+﻿namespace NetPay.DataProcessor
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Data;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data format!";
+        private const string DuplicationDataMessage = "Error! Data duplicated.";
+        private const string SuccessfullyImportedHousehold = "Successfully imported household. Contact person: {0}";
+        private const string SuccessfullyImportedExpense = "Successfully imported expense. {0}, Amount: {1}";
+
+        public static string ImportHouseholds(NetPayContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportExpenses(NetPayContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsValid(object dto)
+        {
+            ValidationContext validationContext = new ValidationContext(dto);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator
+                .TryValidateObject(dto, validationContext, validationResults, true);
+
+            return isValid;
+        }
+    }
+}
---
+++ E08__Exam_Preparation/NetPay/DataProcessor/Serializer.cs
@@ -0,0 +1,17 @@
+﻿namespace NetPay.DataProcessor
+{
+    using Data;
+
+    public class Serializer
+    {
+        public static string ExportHouseholdsWhichHaveExpensesToPay(NetPayContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportAllServicesWithSuppliers(NetPayContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
